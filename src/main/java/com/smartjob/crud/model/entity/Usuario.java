49c00d9a18d2a48ba295a021a96c70911b41f058
CRISTIAN1993UCV <145594124+CRISTIAN1993UCV@users.noreply.github.com>
2023-12-01 04:59:08 -0500

    Add files via upload

--- src/main/java/com/smartjob/crud/model/entity/Usuario.java
+++ src/main/java/com/smartjob/crud/model/entity/Usuario.java
@@ -41,5 +41,4 @@ public class Usuario {
 
     private boolean isActive;
 
-    // Constructores, Getters y Setters
 }
--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,10 +1,20 @@
-CREATE TABLE Usuario (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255),
-    apellido_paterno VARCHAR(255),
-    apellido_materno VARCHAR(255),
-    fecha_nacimiento DATE,
-    sexo VARCHAR(10),
-    correo VARCHAR(255),
-    telefono VARCHAR(20)
+CREATE TABLE usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created DATETIME,
+    modified DATETIME,
+    last_login DATETIME,
+    token VARCHAR(255),
+    is_active BOOLEAN
+);
+
+CREATE TABLE phone (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    number VARCHAR(255),
+    city_code VARCHAR(255),
+    country_code VARCHAR(255),
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES usuario (id) ON DELETE CASCADE
 );
